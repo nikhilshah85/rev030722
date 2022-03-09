dd5670424ac635f914b0b75f5b912bc65b74ce40
Nikhil <nikhilshah.net@hotmail.com>
2022-03-09 12:37:30 -0500

    First OOPS program - Abstraction and Encapsulation

---
+++ day3/oops_1/Accounts.cs
@@ -0,0 +1,56 @@
+using System;
+
+class Accounts
+{
+    int v_accNo;
+    string v_accName;
+    double v_accBalance;
+    bool v_isAccountActive;
+
+    public int v_intrestRate;
+
+
+    public int AccountNumber
+    {
+        get{ return v_accNo;}
+        set{v_accNo  = value;}
+    }
+
+    public string AccountName
+    {
+        get{return v_accName;}
+        set{v_accName = value;}
+    }
+
+    public double AccountBalance
+    {
+        get{return v_accBalance;}
+        set{v_accBalance = value;}
+    }
+
+    public bool isAccountActive
+    {
+        get{return v_isAccountActive;}
+        set{v_isAccountActive = value;}
+    }
+
+
+    public double Widraw(int w_amount)
+    {
+        AccountBalance = AccountBalance - w_amount;
+        return AccountBalance;
+    }
+
+    public double Deposit(int d_amount)
+    {
+        AccountBalance = AccountBalance + d_amount;
+        return AccountBalance;
+    }
+
+    public double CheckBalance()
+    {
+        return AccountBalance;
+    }
+
+
+}
---
+++ day3/oops_1/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace oops_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Accounts accObj1 = new Accounts();
+            accObj1.AccountNumber = 101;
+            accObj1.AccountName = "Nik";
+            accObj1.AccountBalance = 8000;
+            accObj1.isAccountActive = true;
+
+            Console.WriteLine("1. Widraw");
+            Console.WriteLine("2. Deposit");
+            Console.WriteLine("3. Check Balance");
+
+            bool continueBanking = true;
+
+            while(continueBanking)
+            {
+
+            int choice = Convert.ToInt32(Console.ReadLine());
+            switch(choice)
+            {
+                
+                case 1:
+                     Console.WriteLine("Balance Before Widrawal " + accObj1.AccountBalance);
+                     System.Console.WriteLine("Enter the amount you wish to widraw");
+                     int w_amount = Convert.ToInt32(Console.ReadLine());
+                     accObj1.Widraw(w_amount);
+                     Console.WriteLine("Balance After Widraw : " + accObj1.AccountBalance);
+                     break;
+                case 2:
+                     System.Console.WriteLine(" ---------------------------------------------  ");
+                     System.Console.WriteLine("Balance before Deposit " + accObj1.AccountBalance);
+                     System.Console.WriteLine("Enter the amount you wish to Deposit");
+                      int d_amount = Convert.ToInt32(Console.ReadLine());
+                     accObj1.Deposit(d_amount);
+                     System.Console.WriteLine("Balance After Deposit " +  accObj1.AccountBalance);
+                     break;
+                case 3:
+                    System.Console.WriteLine("Available Balance is " + accObj1.CheckBalance());
+                    break;
+                default:
+                    System.Console.WriteLine("Sorry Wrong Input");
+                    continueBanking = false;
+                    break;
+            }
+            }
+           
+
+           
+
+
+        }
+    }
+}
