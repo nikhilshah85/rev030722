815d52ae26bf22d2104847a4d07df4abb23f1891
Nikhil <nikhilshah.net@hotmail.com>
2022-03-10 10:26:47 -0500

    Method Overload

---
+++ day4/method_overLoading/Calculations.cs
@@ -0,0 +1,24 @@
+using System;
+
+class Calculations
+{
+    public int Add(int num1, int num2)
+    {
+        return num1 + num2;
+    }
+
+    public double Add(double num1, double num2)
+    {
+        return num1 + num2;
+    }
+
+    public int Add(int num1, int num2, int num3)
+    {
+        return num1 + num2 + num3;
+    }
+
+    public int Add(int num1, int num2, int num3, int num4)
+    {
+        return num1 + num2 + num3 + num4;
+    }
+}
---
+++ day4/method_overLoading/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+
+
+Calculations calObj = new Calculations();
+int addResult  = calObj.Add(20,30);
+
+Console.WriteLine(" 2 Numbers Addition " + addResult);
+Console.WriteLine(" 3 Numbers Addition " + calObj.Add(30,30,30));
+Console.WriteLine(" 4 Numbers Addition " + calObj.Add(30,30,30,40));
+Console.WriteLine(" 2 Doubles Addition " + calObj.Add(310.5,60.65));
