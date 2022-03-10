3c7db65629459e17e212eb398de1e3832511322e
Nikhil <nikhilshah.net@hotmail.com>
2022-03-10 10:56:13 -0500

    Params Array

---
+++ day4/method_demo/Calculations.cs
@@ -0,0 +1,39 @@
+using System;
+
+
+class Calculations
+{
+
+    public int Add(int num1, int num2, params int[] moreNumbers)
+    {
+        int temp = 0;
+        for (int i = 0; i < moreNumbers.Length ; i++)
+        {
+            temp += moreNumbers[i];
+        }
+        return num1 + num2 + temp;
+    }
+
+
+
+
+    // public int Add(int num1, int num2)
+    // {
+    //     return num1 + num2;
+    // }
+
+    // public double Add(double num1, double num2)
+    // {
+    //     return num1 + num2;
+    // }
+
+    // public int Add(int num1, int num2, int num3)
+    // {
+    //     return num1 + num2 + num3;
+    // }
+
+    // public int Add(int num1, int num2, int num3, int num4)
+    // {
+    //     return num1 + num2 + num3 + num4;
+    // }
+}
---
+++ day4/method_demo/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace method_demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculations calObj  = new Calculations();
+            // Console.WriteLine(calObj.Add(2,5));
+
+            Console.WriteLine("Enter first Number");
+            int num1  = Convert.ToInt32(Console.ReadLine());
+
+             Console.WriteLine("Enter first Number");
+            int num2  = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Pass more numbers, Press 0 when done to see the addition");
+
+            int temp = 1;
+            int moreNumbers = 0;
+            while (temp != 0)
+            {
+
+                temp = Convert.ToInt32(Console.ReadLine());
+                moreNumbers += temp;               
+            }
+
+            Console.Clear();
+            Console.WriteLine("Addition of All your numbers are " + calObj.Add(num1,num2,moreNumbers));
+
+
+          
+           
+            
+
+
+
+
+
+        }
+    }
+}
