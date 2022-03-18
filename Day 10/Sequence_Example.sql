98e523c11246552c3ea10512b8b1aadc9a3c3b4f
Nikhil <nikhilshah.net@hotmail.com>
2022-03-18 12:08:45 -0400

    Sequence demo

---
+++ Day 10/Sequence_Example.sql
@@ -0,0 +1,52 @@
+create database testDB3
+
+use testDB3
+
+create table tbl_chineseDishOrders
+(
+	orderId int primary key,
+	dishName_1 varchar(20),
+	dishName_2 varchar(20),
+	dishName_3 varchar(20),
+	dishName_4 varchar(20)
+	)
+
+create table tbl_ItalianDishOrders
+(
+	orderId int primary key,
+	dishName_1 varchar(20),
+	dishName_2 varchar(20),
+	dishName_3 varchar(20),
+	dishName_4 varchar(20)
+	)
+
+create table tbl_IceCreamOrders
+(
+	orderId int primary key,
+	dishName_1 varchar(20),
+	dishName_2 varchar(20),
+	dishName_3 varchar(20),
+	dishName_4 varchar(20)
+	)
+
+select * from tbl_chineseDishOrders
+
+
+create sequence seq_orderId
+start with 200
+increment by 2
+
+--test the sequence 
+select Next value for seq_orderId
+
+insert into tbl_IceCreamOrders values (next value for seq_orderId,'Ice 1','Ice 2','Ice 3','Ice 4')
+select * from tbl_IceCreamOrders
+
+
+insert into tbl_chineseDishOrders values (next value for seq_orderId,'Ch 1','CH 2','CH 3','CH 4')
+
+insert into tbl_ItalianDishOrders values (next value for seq_orderId,'ITY 1','ITY 2','ITY 3','ITY 4')
+
+select * from tbl_IceCreamOrders
+select * from tbl_chineseDishOrders
+select * from tbl_ItalianDishOrders
