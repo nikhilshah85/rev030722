009b241874cabf1755949c0175ff6db536e9217a
Nikhil <nikhilshah.net@hotmail.com>
2022-03-24 12:07:42 -0400

    Product API - GET Methods

---
+++ day14/product_webapi/product_webapi/Controllers/ProductsController.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using product_webapi.Models;
+namespace product_webapi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        #region First Greet Method
+        //[HttpGet]
+        //public IActionResult Greet()
+        //{
+        //    return Ok("Welcome to Web API - RESTFUL Services");
+        //}
+        #endregion
+
+        #region Pass a Parmeter
+
+        //[HttpGet]
+        //public IActionResult GreetUser(string guestName)
+        //{
+        //    return Ok("Welcome : " + guestName);
+        //}
+        #endregion
+
+        #region Return Products Array
+        //[HttpGet]
+        //public IActionResult GetProductList()
+        //{
+        //    string[] products = new string[10];
+        //    products[0] = "Pepsi";
+        //    products[1] = "Coke";
+        //    products[2] = "IPhone";
+        //    products[3] = "Fossil";
+        //    products[4] = "Dell Lattitude";
+        //    products[5] = "Burger";
+        //    products[6] = "Pizza";
+        //    products[7] = "Strawberry Milk Shake";
+        //    products[8] = "Mango Shake";
+        //    products[9] = "Air Pods";
+        //    return Ok(products);            
+        //}
+        #endregion
+
+
+        #region Product Info
+        //[HttpGet]
+        //public IActionResult GetProductInformation()
+        //{
+        //    ProductsModel model = new ProductsModel();
+        //    return Ok(model.GetProductInfo());
+        //}
+        #endregion
+
+        #region Product list
+        [HttpGet]
+        public IActionResult ProductList()
+        {
+            ProductsModel model = new ProductsModel();
+            return Ok(model.GetProductlist());
+        }
+
+        #endregion
+
+    }
+}
---
+++ day14/product_webapi/product_webapi/Models/ProductsModel.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.Generic;
+
+namespace product_webapi.Models
+{
+    public class ProductsModel
+    {
+        #region Product Properties
+        public int pId { get; set; }
+        public string pName { get; set; }
+        public double pPrice { get; set; }
+        public bool pIsInStock { get; set; }
+        public string pCategory { get; set; }
+        public int pAvailableQty { get; set; }
+        
+        #endregion
+
+        public ProductsModel GetProductInfo()
+        {
+            //we will get the values from database
+            return new ProductsModel()
+            {
+                pId = 101,
+                pName = "Green Tea",
+                pPrice = 8,
+                pIsInStock = true,
+                pCategory = "Health Drink",
+                pAvailableQty = 20
+            };
+        }
+
+
+
+        public List<ProductsModel> GetProductlist()
+        {
+            List<ProductsModel> products = new List<ProductsModel>();
+            products.Add(new ProductsModel() { pId=101, pName="Pepsi", pCategory="Cold-Drink", pAvailableQty=20, pIsInStock=true, pPrice=50 });
+            products.Add(new ProductsModel() { pId=102, pName="Coke", pCategory="Cold-Drink", pAvailableQty=20, pIsInStock=true, pPrice=50 });
+            products.Add(new ProductsModel() { pId=103, pName="Maggie Noodles", pCategory="Fast-Food", pAvailableQty=20, pIsInStock=true, pPrice=50 });
+            products.Add(new ProductsModel() { pId=104, pName="Burger", pCategory="Fast-Food", pAvailableQty=20, pIsInStock=false, pPrice=50 });
+            products.Add(new ProductsModel() { pId=105, pName="Pizza", pCategory="Fast-Food", pAvailableQty=20, pIsInStock=true, pPrice=50 });
+            products.Add(new ProductsModel() { pId=106, pName="Iphone", pCategory="Smart-Phone", pAvailableQty=20, pIsInStock=true, pPrice=50 });
+            products.Add(new ProductsModel() { pId=107, pName="Mac book Pro", pCategory="Smart-PC", pAvailableQty=20, pIsInStock=true, pPrice=50 });
+            products.Add(new ProductsModel() { pId=108, pName="Ipad Air", pCategory="Handy Pad", pAvailableQty=20, pIsInStock=false, pPrice=50 });
+            products.Add(new ProductsModel() { pId=109, pName="Airpods", pCategory="Wireless", pAvailableQty=20, pIsInStock=true, pPrice=50 });
+            products.Add(new ProductsModel() { pId=110, pName="Dell", pCategory="Laptop", pAvailableQty=20, pIsInStock=true, pPrice=50 });
+            products.Add(new ProductsModel() { pId=111, pName="Green Tea", pCategory="Health-Drink", pAvailableQty=20, pIsInStock=false, pPrice=50 });
+            return products;
+        }
+        
+    }
+
+
+}
