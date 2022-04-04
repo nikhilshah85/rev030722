d426ca1c4befbb18fd64c21a68768729fc6a6cbe
Nikhil <nikhilshah.net@hotmail.com>
2022-04-04 12:04:27 -0400

    REST Call - JQuery

---
+++ day19/consume_RestAPI/consume_RestAPI/Controllers/ClientsideAPICallController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace consume_RestAPI.Controllers
+{
+    public class ClientsideAPICallController : Controller
+    {
+
+        public IActionResult GetRESTDataClient()
+        {
+            return View();
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
